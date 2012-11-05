18047c7fdb887037c62a09af628a42dfdb8ac312
Armin Jenewein <netzverweigerer@gmail.com>
2012-11-05 23:32:12 +0100

    output a nice list containing hostname (if known) and IP

--- hoehlenstatus.py
+++ hoehlenstatus.py
@@ -6,10 +6,24 @@ e = execute('sudo nmap -sP 172.31.97.0/24')
 output = e.stdout
 
 lines = output.split('\n')
+lines.append('')
 
+linenum = 0
 for line in lines:
-    if line.startswith('Nmap scan report for'):
-        hostname = line.split('Nmap scan report for ')[1].split(' ')[0]
-        print "Hostname: " + hostname
+    linenum +=1
+    if 'is up' in line:
+        hostline_full = lines[linenum-2].replace('Nmap scan report for ','')
+        if "(" in hostline_full:
+            h = hostline_full.split(' ')
+            host_hostname = h[0]
+            host_ip = h[1]
+        else:
+            host_hostname = "UNKNOWN"
+            host_ip = hostline_full
+        print "--------------"
+        print host_hostname
+        print host_ip
+
+
 
 
