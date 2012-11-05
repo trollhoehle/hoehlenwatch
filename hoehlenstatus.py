879830da6a061b6513bcdafd3575eadc29e85cc3
Armin Jenewein <netzverweigerer@gmail.com>
2012-11-05 23:42:04 +0100

    now shows mac address as well

--- hoehlenstatus.py
+++ hoehlenstatus.py
@@ -16,14 +16,18 @@ for line in lines:
         if "(" in hostline_full:
             h = hostline_full.split(' ')
             host_hostname = h[0]
-            host_ip = h[1]
+            host_ip = h[1].replace('(','').replace(')','')
         else:
-            host_hostname = "UNKNOWN"
+            host_hostname = "unknown_hostname"
             host_ip = hostline_full
+        mac = lines[linenum].split(' ')[2:][0]
+        if not ":" in mac:
+            mac = "unknown_mac"
         print "--------------"
         print host_hostname
         print host_ip
-
+        print mac
+        print ""
 
 
 
