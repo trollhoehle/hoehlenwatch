bb5b837a0dff532586ef0e4c7be60107e912f04e
Armin Jenewein <netzverweigerer@gmail.com>
2012-11-06 00:15:23 +0100

    schreibt jetzt auch last_seen timestamp in die sqlite db, bitch

--- hoehlenstatus.py
+++ hoehlenstatus.py
@@ -14,8 +14,9 @@ e = execute('sudo nmap -sP 172.31.97.0/24')
 
 import sqlite3
 
-CREATE_SQL = """CREATE TABLE IF NOT EXISTS status(mac PRIMARY KEY, ip VARCHAR(128), hostname VARCHAR(128));"""
-INSERT_SQL = """INSERT INTO status (mac, ip, hostname) VALUES (?, ?, ?);"""
+CREATE_SQL = """CREATE TABLE IF NOT EXISTS status(mac PRIMARY KEY, ip VARCHAR(128), hostname VARCHAR(128), last_seen datetime);"""
+INSERT_SQL = """INSERT INTO status (mac, ip, hostname, last_seen) VALUES (?, ?, ?, datetime('now'));"""
+UPDATE_SQL = """UPDATE status SET last_seen=datetime('now') WHERE mac = ?;"""
 
 conn = sqlite3.connect('example.db')
 conn.execute(CREATE_SQL)
@@ -45,5 +46,10 @@ for line in lines:
         print host_ip
         print mac
         print ""
-    conn.execute(INSERT_SQL, (mac, host_ip, host_hostname))
+        try:
+            conn.execute(INSERT_SQL, (mac, host_ip, host_hostname))
+        except sqlite3.IntegrityError:
+            # We already know this mac, so we update the last_seen timestamp
+            conn.execute(UPDATE_SQL, (mac,))
+conn.commit()
 
