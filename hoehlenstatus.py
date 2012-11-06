b08cfd1f5d2006156ea1861bb30d894a4f025fde
Armin Jenewein <netzverweigerer@gmail.com>
2012-11-06 20:54:48 +0100

    added nice comments rename: example.db -> status.db added status.db to gitignore

--- hoehlenstatus.py
+++ hoehlenstatus.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python2
 
+### sqlite:
+# to initialize the database table:
 # sqlite> CREATE TABLE status(mac PRIMARY KEY,ip VARCHAR(128),hostname VARCHAR(128));
+# to view the schema:
 # sqlite> .schema
-# CREATE TABLE status(mac PRIMARY KEY,ip VARCHAR(128),hostname VARCHAR(128));
+# to insert a row into the status table:
 # sqlite> INSERT INTO status (mac,ip,hostname) VALUES ('zoranhatnenWIRKLICHkurzenpenis','127.0.0.1','zoranstollewindowskiste');
-# sqlite> select * from status;
-# zoranhatnenWIRKLICHkurzenpenis|127.0.0.1|zoranstollewindowskiste
-# sqlite> delete from status where mac = 'zoranhatnenWIRKLICHkurzenpenis';
-# sqlite> 
 
 from execute import execute
+
+print "Fetching results from nmap..."
 e = execute('nmap -sP 172.31.97.0/24')
+print "nmap run done"
 
 import sqlite3
 
@@ -18,7 +20,7 @@ CREATE_SQL = """CREATE TABLE IF NOT EXISTS status(mac PRIMARY KEY, ip VARCHAR(12
 INSERT_SQL = """INSERT INTO status (mac, ip, hostname, last_seen) VALUES (?, ?, ?, datetime('now'));"""
 UPDATE_SQL = """UPDATE status SET last_seen=datetime('now') WHERE mac = ?;"""
 
-conn = sqlite3.connect('example.db')
+conn = sqlite3.connect('status.db')
 conn.execute(CREATE_SQL)
 
 output = e.stdout
